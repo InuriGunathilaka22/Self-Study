cd2eaa8464848c34704cf222ef11367fb4d00809
InuriGunathilaka22 <inurilee99@gmail.com>
2023-08-11 23:33:36 +0530

    Bulb off , Bulb On

---
+++ JS/day 01/main.js
@@ -0,0 +1,71 @@
+// alert("Hello World");
+// console.log('Hello World!');
+// console.error('This is an Error!')
+
+
+// let m = 11;
+// m =12;
+
+// console.log(m);
+
+// const y = 12;
+// y = 50;
+
+// console.log(y);
+
+
+//Arrays
+// const numbers = new Array(1,2,3,4,5);
+
+// console.log(numbers);
+
+//Arrays
+// const fruits = new Array('Mango','Banana','Grapes','Apple', 78, true);
+
+// console.log(fruits);
+
+// const mix = ['you', 'me', 56, false];
+
+// console.log(mix);
+
+// const me = ['Inuri', 24, 5, 'macaroni', 'kbv', 'usj', 'IT']
+// me[7] = "to watch films";
+
+// console.log(me)
+
+
+// const person = {
+//     firstName : 'John',
+//     lastName : 'Doe',
+//     age : 29,
+//     hobbies : ['music', 'movies', 'sports'],
+//     address: {
+//         street:'somewhere in the world',
+//         city: 'New York',
+//         state: 'NY'
+//     }
+
+// }
+
+// console.log(person.firstName, person.lastName);
+
+// console.log(person.hobbies[1]);
+
+// console.log(person.address.city)
+
+
+
+
+//for
+
+for (let i = 0; i < 10; i++) {
+    console.log (`For loop number:${i}`);
+}
+
+//while
+
+let i = 0;
+while (i < 10) {
+    console.log (`While loop number:${i}`);
+    i++;
+}
